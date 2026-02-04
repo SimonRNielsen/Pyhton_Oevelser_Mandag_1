89b823d5edaac3baa4eb43a64595cef8629f133b
SimonRNielsen <simonrn@gmail.com>
2026-02-04 10:48:17 +0100

    commit

--- Opgave5.py
+++ Opgave5.py
@@ -10,25 +10,26 @@ def create_word(answer: str):
     if not (set(answer).issubset(letters)):
         print("Ugyldigt ord, må kun indeholde gyldige bogstaver!")
         return 0
-    if (correct_answers.__contains__(answer)):
-        print("Du har allerede gættet dette ord")
+    if (answer in correct_answers):
+        print("Du har allerede gættet dette ord!")
         return 0
-    elif (valid_answers.__contains__(answer)):
+    elif (answer in valid_answers):
         correct_answers.append(answer)
+        print("Godtaget svar!")
         return answer.__len__()
     else:
-        print("Ordet er ikke en gyldig svarmulighed")
+        print("Ordet er ikke en gyldig svarmulighed!")
         return 0
 
 while (len(valid_answers) != len(correct_answers) and playing):
     if (len(correct_answers) > 0):
-        print("Du har gættet følgende ord så vidt:", correct_answers)
+        print("Du har gættet følgende ord så vidt:", correct_answers, f"\nDer mangler at blive gættet {len(valid_answers) - len(correct_answers)} af de angivne muligheder")
     print("Lav et ord ud fra følgende bogstaver eller skriv \"exit for at stoppe\":", letters)
     score = create_word(input().upper())
     if (score < 0):
         playing = False
     else:
         total_score += score
-print(f"Du opnåede en score på: {total_score}")
+print("Du opnåede en score på:", total_score)
 if (len(valid_answers) == len(correct_answers)):
     print("Du gættede alle mulighederne!")
