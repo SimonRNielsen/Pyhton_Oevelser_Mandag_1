dafffbcbdfb8a43171396315cacdb887dbad04a2
SimonRNielsen <simonrn@gmail.com>
2026-02-04 09:20:05 +0100

    commit

--- Opgave4.py
+++ Opgave4.py
@@ -1,18 +1,13 @@
-question_one = { "Ja": True, "Nej": False, "Næh": False, "Absolut ikke": False}
-question_two = { "Ja": True, "Nej": False, "Næh": False, "Absolut ikke": False}
-question_three = { "Ja": True, "Nej": False, "Næh": False, "Absolut ikke": False}
-questions = { "Er dette en quiz?": question_one, "Er det en træls opgave?": question_two, "Skal den bare hurtig overstås?": question_three }
+replies = { "Ja": True, "Nej": False, "Næh": False, "Absolut ikke": False}
+questions = { "Er dette en quiz?": replies, "Er det en træls opgave?": replies, "Skal den bare hurtig overstås?": replies }
 correct = 0
 wrong = 0
 
 def ask_question(question):
     answers = questions[question]
     print(f"{question} - svarmuligheder: {list(answers.keys())}")
-    answer = input("Vælg et svar:\n")
-    if (questions[question][answer] is True):
-        return True
-    else:
-        return False
+    answer = input("Vælg et svar:\n").capitalize()
+    return questions[question][answer]
 
 for i in questions:
     if (ask_question(i) is True):
